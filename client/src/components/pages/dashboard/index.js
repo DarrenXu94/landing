2f57cdef190d2fd79303741ef0628f1c7266deac
Darren <darrenxu1994@gmail.com>
2018-09-08 17:11:35 +1000

    styled components

--- client/src/components/pages/dashboard/index.js
+++ client/src/components/pages/dashboard/index.js
@@ -5,18 +5,14 @@ import BodyRowStyle from './styling/BodyRowStyle';
 import PanelRow from './PanelRow'
 import { Column } from 'simple-flexbox';
 import Fill from '../../styling/Fill'
-import './style.css'
 
 // Components to load
 import ChromeRecent from './features/ChromeRecent'
 import Notepad from './features/Notepad'
 import KeywordFeature from './features/KeywordFeature'
 
-// const h1DivStyle = {
-//     position: 'relative',
-//     display: 'inline-block',
-//     overflow: 'hidden',
-// }
+import HeadingStyle from './styling/HeadingStyle'
+
 
 class DashboardLanding extends Component {
     render() {
@@ -26,9 +22,9 @@ class DashboardLanding extends Component {
                     <Fragment></Fragment>
                 </TopPanel>
                 <HeaderRow>
-                    <div className='anim'>
-                        <h1 className='animh1'>What would you like to do?</h1>
-                    </div>
+                    <HeadingStyle>
+                        <h1>What would you like to do?</h1>
+                    </HeadingStyle>
                 </HeaderRow>
                 <BodyRowStyle horizontal='center'>
                     <Column style={{justifyContent: "space-around", width: "100%"}}>
---
+++ client/src/components/pages/dashboard/styling/HeadingStyle.js
@@ -0,0 +1,55 @@
+import styled, {keyframes} from "styled-components"
+
+const showText = keyframes`
+0% {
+    width: 0;
+    left: 0;
+}
+50% {
+    width: 100%;
+    left: 0
+}
+100% {
+    width: 0;
+    left: 100%;
+}
+`;
+
+const animateText = keyframes`
+from {
+    opacity: 0;
+}
+to {
+    opacity: 1;
+}
+`
+
+export default styled.div`
+    position: relative;
+    display: inline-block;
+    overflow: hidden;
+    h1 {
+        opacity: 0;
+        animation: ${animateText} .7s forwards;
+    }
+    ::before, ::after {
+        content: '';
+        position: absolute;
+        top:0; right:0;
+        height: 100%;
+        z-index: 1;
+        -webkit-clip-path: polygon(25% 0%, 100% 0%, 100% 100%, 0% 100%);
+        clip-path: polygon(25% 0%, 100% 0%, 100% 100%, 0% 100%);
+    }
+    ::before{
+        background-color: rgba(198,160,135,1);
+        animation: ${showText} .8s ease;
+    }
+    
+    ::after{
+        background-color: black;
+        animation-delay: .5s;
+        animation: ${showText} .6s ease;
+    }
+`
+
